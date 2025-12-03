d72c3a3789216f46ff24272f3262f2d4837ec318
Alok <69.uralok@gmail.com>
2025-12-03 23:30:57 +0530

    Added some handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,21 @@
 package main
 
-import(
+import (
 	// "fmt"
-	"github.com/gin-gonic/gin"
+	"log"
+
 	"github.com/TheAlok15/collab/internal/database"
+	"github.com/TheAlok15/collab/internal/handlers"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main(){
+	err := godotenv.Load()
+if err != nil {
+    log.Println("No .env file found")
+}
+
 	r := gin.Default()
 
 	database.ConnectDB()
@@ -15,6 +24,8 @@ func main(){
 		c.JSON(200, gin.H{"message": "Hello Alok Go backend running!"})
 		c.JSON(501, gin.H{"status code" : 100})
 	})
+	r.POST("/signup",handlers.Signup)
+	r.POST("/sigin",handlers.Signin)
 
 	r.Run(":8080")
 }
--- internal/database/db.go
+++ internal/database/db.go
@@ -14,11 +14,11 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	host := "127.0.0.1"
-	port := "5432"
-	user := "admin"
-	password := "admin"
-	dbname := "collab"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",host,port,user,password,dbname)
 
