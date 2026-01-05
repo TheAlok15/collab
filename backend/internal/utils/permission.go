2428658b686e680f062df1bd2ec23497d1b9596a
Alok <69.uralok@gmail.com>
2026-01-05 22:54:44 +0530

    feat: add document permission and share link access control

--- backend/internal/utils/permission.go
+++ backend/internal/utils/permission.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"time"
 
 	"github.com/TheAlok15/collab/internal/database"
 	"github.com/TheAlok15/collab/internal/models"
@@ -30,3 +32,90 @@ func IsCollaborator(userID uint, docID uint) (bool,string, error){
 
 	return true, collab.Permission, nil
 }
+
+func GetShareLink(token string) (*models.ShareLink, error) {
+    var link models.ShareLink
+
+    if err := database.DB.Where("token = ?", token).First(&link).Error; err != nil {
+        return nil, err
+    }
+
+    if !link.IsEnabled {
+        return nil, errors.New("share link is disabled")
+    }
+
+    if link.ExpirationTime != nil && time.Now().After(*link.ExpirationTime) {
+        return nil, errors.New("share link expired")
+    }
+
+    return &link, nil
+}
+
+func CanRead(userID uint, docID uint, token string) (bool, error) {
+
+    isOwner, err := IsOwner(userID, docID)
+    if err != nil {
+        return false, err
+    }
+    if isOwner {
+        return true, nil
+    }
+
+    isCollab, perm, _ := IsCollaborator(userID, docID)
+    if isCollab {
+        return true, nil
+    }
+
+    if token != "" {
+        link, err := GetShareLink(token)
+        if err != nil {
+            return false, nil
+        }
+
+        if link.IsPublic && link.AllowRead {
+            return true, nil
+        }
+
+        if !link.IsPublic && link.AllowRead && userID != 0 {
+            return true, nil
+        }
+    }
+
+    return false, nil
+}
+
+func CanEdit(userID uint, docID uint, token string) (bool, error) {
+
+    isOwner, err := IsOwner(userID, docID)
+    if err != nil {
+        return false, err
+    }
+    if isOwner {
+        return true, nil
+    }
+
+    isCollab, perm, _ := IsCollaborator(userID, docID)
+    if isCollab && perm == "edit" {
+        return true, nil
+    }
+
+    if token != "" {
+        link, err := GetShareLink(token)
+        if err != nil {
+            return false, nil
+        }
+
+        if link.IsPublic && link.AllowEdit {
+            return true, nil
+        }
+
+        if !link.IsPublic && link.AllowEdit && userID != 0 {
+            return true, nil
+        }
+    }
+
+    return false, nil
+}
+
+
+
